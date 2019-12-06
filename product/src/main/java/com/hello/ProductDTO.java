22aad19281978af65ecb411856b299206b98548b
Anand <anand@AnandSingh-100407.dexcom.dexcominc.com>
2019-12-06 14:11:15 -0800

    initial commit

---
+++ product/src/main/java/com/hello/ProductDTO.java
@@ -0,0 +1,35 @@
+package com.hello;
+
+public class ProductDTO {
+	
+	private String review1;
+	private String review2;
+	private String review3;
+	private String details;
+	
+	public String getReview1() {
+		return review1;
+	}
+	public void setReview1(String review1) {
+		this.review1 = review1;
+	}
+	public String getReview2() {
+		return review2;
+	}
+	public void setReview2(String review2) {
+		this.review2 = review2;
+	}
+	public String getReview3() {
+		return review3;
+	}
+	public void setReview3(String review3) {
+		this.review3 = review3;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+}
---
+++ rattings/src/main/java/com/hello/RattingsApplication.java
@@ -0,0 +1,13 @@
+package com.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RattingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RattingsApplication.class, args);
+	}
+
+}
