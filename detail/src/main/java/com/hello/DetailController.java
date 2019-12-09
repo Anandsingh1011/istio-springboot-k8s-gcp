6bb88bbdb5a0c08ad38205359829b759295e6982
Anand <anand@AnandSingh-100407.dexcom.dexcominc.com>
2019-12-09 10:12:25 -0800

    Loging added

--- detail/src/main/java/com/hello/DetailController.java
+++ detail/src/main/java/com/hello/DetailController.java
@@ -1,5 +1,7 @@
 package com.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RestController
 public class DetailController {
+	
+	private static final Logger log = LoggerFactory.getLogger(DetailController.class);
 
 	@RequestMapping(value = "/details", method = RequestMethod.GET)
 	public String findDetail(String productName) {
-		System.out.println("findDetail called : " + productName);
+		log.info("findDetail called : " + productName);
 		String detail = "This is very good project " + productName + " !";
 		return detail;
 
--- product/src/main/java/com/hello/ProductController.java
+++ product/src/main/java/com/hello/ProductController.java
@@ -1,5 +1,6 @@
 package com.hello;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,15 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
 
+	private static final Logger log = LoggerFactory.getLogger(ProductController.class);
+	
 	@Autowired
 	private ExternalService remoteService;
 
 	@RequestMapping(value = "/product/{productName}", method = RequestMethod.GET)
 	public ProductDTO getProductDetail(@PathVariable("productName") String productName) {
 
+		log.info("getProductDetail called  : " + productName);
 		ProductDTO dto = new ProductDTO();
 
-		String detail = remoteService.getDetails(productName);
+		String detail = "";
+		
+		try {
+			detail = remoteService.getDetails(productName);
+		} catch (Exception e) {
+			log.error("Error calling remoteService.getDetails " + productName);
+			e.printStackTrace();
+		}
 
 		dto.setName(productName);
 		dto.setDetails(detail);
