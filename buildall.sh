fe2fff7a161d8d6450c590a27d6eb1e33dc3cf3f
Anand <Anandsingh1011@users.noreply.github.com>
2019-12-10 11:33:58 -0800

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -1,14 +1,41 @@
 #!/bin/sh
 
+echo '===================================================================================='
+echo '      Start Build code, build docker image and push to registery'
+echo '===================================================================================='
+
 cd product/
 mvn clean install
 docker build -t gcr.io/project-vnv/product:v1 .
 docker push gcr.io/project-vnv/product:v1
 
 
-cd detail/
+cd ../detail/
 mvn clean install
 docker build -t gcr.io/project-vnv/detail:v1 .
 docker push gcr.io/project-vnv/detail:v1
 
-echo 'All Build Done'
+cd ../rettings/
+mvn clean install
+docker build -t gcr.io/project-vnv/rettings:v1 .
+docker push gcr.io/project-vnv/rettings:v1
+
+
+cd ../review-1/
+mvn clean install
+docker build -t gcr.io/project-vnv/review-1:v1 .
+docker push gcr.io/project-vnv/review-1:v1
+
+cd ../review-2/
+mvn clean install
+docker build -t gcr.io/project-vnv/review-2:v1 .
+docker push gcr.io/project-vnv/review-2:v1
+
+cd ../review-3/
+mvn clean install
+docker build -t gcr.io/project-vnv/review-3:v1 .
+docker push gcr.io/project-vnv/review-3:v1
+
+echo '===================================================================================='
+echo '                              All Build Done                                        '
+echo '===================================================================================='
